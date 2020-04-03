62f3f80e6ea3d626a738b15886cc1dca1bd86cd6
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-03 17:17:56 -0400

    seting  mLab

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use(express.static("public"));
 //for heroku mLab
 mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_szpf1gfd:no924vtcjqqjtqce1sn5e24vm8@ds119489.mlab.com:19489/heroku_szpf1gfd", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useMongoClient: true
+  useFindAndModify: false
 });
 
 // routes
